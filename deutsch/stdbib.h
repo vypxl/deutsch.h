edbde4e8f86aac80f9f9392037a72fed5b91519a
umgefahren <55623006+umgefahren@users.noreply.github.com>
2022-07-26 17:05:35 +0200

    Erste schritte bei stdbib

---
+++ deutsch/stdbib.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "stdlib.h"
+
+#define azus atof
+#define azug atoi
+#define azul atol
+#define azull atoll
+#define zekzud strtod
+#define zekzus strtof
+#define zekzul strtol
+#define zekzuld strtold
+#define zekzull strtoll
+#define zekzuul strtoul
+#define zekzuull strtoull
+
+#define zufall rand
+#define samezufall srand
+
+#define nullvergebe calloc
+#define befreie free
+#define vergebe malloc
+#define vergebeneu realloc
+
+#define brecheab abort
+#define beimverlassen atexit
+#define beim_schnellen_verlassen at_quick_exit
+#define verlassen exit
+#define holeumgebung getenv
+#define verlasse_schnell quick_exit
+// system ist auch auf deutsch system
